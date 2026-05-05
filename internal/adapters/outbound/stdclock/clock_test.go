b7c019228a94ad46e4f090eb82a3b9f8337bd967
Russell <russellvergara@hotmail.com>
2026-05-05 10:51:40 -0500

    feat(adapters): add stdclock

---
+++ internal/adapters/outbound/stdclock/clock_test.go
@@ -0,0 +1,21 @@
+package stdclock_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/RVRTelecomunicaciones/sophia-cli/internal/adapters/outbound/stdclock"
+	"github.com/RVRTelecomunicaciones/sophia-cli/internal/ports/outbound"
+)
+
+func TestStdClockImplementsClock(t *testing.T) {
+	var _ outbound.Clock = stdclock.New()
+}
+
+func TestStdClockNowIsRecent(t *testing.T) {
+	c := stdclock.New()
+	now := c.Now()
+	if time.Since(now) > time.Second {
+		t.Errorf("Now() not recent: %v", now)
+	}
+}
